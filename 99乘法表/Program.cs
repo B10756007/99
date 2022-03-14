48f82c958e8526faba9757d62ae601f1d5ac9499
user <user@DESKTOP-6JBEF5K>
2022-03-14 11:41:16 +0800

    99乘法表

--- 99乘法表/Program.cs
+++ 99乘法表/Program.cs
@@ -14,7 +14,7 @@ namespace _99乘法表
                 {
                     Console.WriteLine(i + "*" + j + "=" + i * j );
                 }
-                Console.Write("\r\n");
+                Console.Write("\n");
             }
         }
     }
