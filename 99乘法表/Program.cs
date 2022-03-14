cb3256a6c0f96fca75e2f92b69183dbd927317d0
user <user@DESKTOP-6JBEF5K>
2022-03-14 11:19:54 +0800

    加入專案檔案。

---
+++ 99乘法表/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _99乘法表
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("99乘法表");
+
+            for(int i = 1; i <= 9 ; i++)
+            {
+                for (int j = 1; j<=9; j++)
+                {
+                    Console.WriteLine(i + "*" + j + "=" + i * j );
+                }
+            }
+        }
+    }
+}
